cdde08b0612fdde82bfa56422890942625cda462
esim7 <makishev@yahoo.com>
2019-09-03 14:46:50 +0600

    Загрузка практического задания

---
+++ practical-task-7/MethodException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical_task_7
+{
+    class MethodException
+    {
+        public string Name { get; set; }
+
+        public void FirstMethod()
+        {
+            try
+            {
+                SecondMethod(Name);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"Вызвано исключение: {exception.Message}");
+            }
+        }
+
+        public void SecondMethod(string Name)
+        {
+            string name = Name;
+            if (name == "Makishev")
+            {
+                throw new Exception();
+            }
+            else
+            {
+                Console.WriteLine("Все прошло ОК");
+            }
+        }
+    }
+}
